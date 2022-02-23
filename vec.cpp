13ee3c9f6de10be08c196bc850ae29bb2d6f6bf2
Junjie Feng <simaqingshan147@outlook.com>
2022-02-23 16:37:52 +0800

    vec.cpp update

--- vec.cpp
+++ vec.cpp
@@ -23,8 +23,12 @@ public:
 	//拷贝赋值运算符
 	Vec<T>& operator=(const Vec<T>&);
 	~Vec() { this->free(); };
-
+	//移动构造函数
+	Vec(Vec<T>&&s)noexcept;
+	//移动赋值运算符
+	Vec<T>& operator=(Vec<T>&rhs)noexcept;
 	void push_back(const T&);
+	void push_back(T&&);  //移动版本的push_back
 	size_t size()const { return first_free - element; }
 	size_t capacity()const { return cap - element; }
 	T* begin()const { return element; }
@@ -56,6 +60,14 @@ void Vec<T>::push_back(const T& s) {
 	chk_n_alloc();  //确保有新内存
 	//在first_free指向的元素中构造s的副本
 	alloc.construct(first_free++, s);
+}
+
+template<typename T>
+void Vec<T>::push_back(T&& s){
+	chk_n_alloc();
+	//s也是左值,即使本身是某个值的右值引用
+	//如果不用move,construct仍然会拷贝s
+	alloc.construct(first_free++,std::move(s));
 }
 
 template<typename T>
@@ -106,21 +118,67 @@ void Vec<T>::reallocate() {
 	auto newData = alloc.allocate(newCap);
 	auto dest = newData;  //新vector指针
 	auto elem = element;
+	/*
 	//调用move，则原vector的元素无需拷贝
 	for (size_t i = 0; i != size(); i++)
 		alloc.construct(dest++, std::move(*elem++));
+	*/
+
+	//调用移动迭代器,也无需拷贝原vector
+	//移动迭代器，支持原迭代器所有操作，重构解引用运算符,返回右值引用
+	//将移动迭代器传递给算法
+	dest = uninitialized_copy(make_move_iterator(begin()),
+							make_move_iterator(end()),
+							newData);
+
 	free();  //释放原内存
 	element = newData;
 	first_free = dest;
 	cap = element + newCap;
 }
 
+//移动操作不分配资源，一般不抛出异常
+//noexcept必须在声明和定义中都指定
+template<typename T>
+Vec<T>::Vec(Vec<T>&&s) noexcept  //移动操作不抛出异常
+//成员初始化器接管s的资源
+:element(s.element),first_free(s.first_free),
+	cap(s.cap){
+		//移后源对象不再指向被移动的资源
+		s.element = s.first_free = s.cap = nullptr;
+	}
+
+template<typename T>
+Vec<T>& Vec<T>::operator=(Vec<T>& rhs) noexcept{
+	//直接检查自赋值
+	if(this != &rhs)
+	{
+		free();
+		//接管资源
+		element = rhs.element;
+		first_free = rhs.first_free;
+		cap = rhs.cap;
+		//rhs置于可析构状态
+		rhs.element = rhs.first_free = rhs.cap = nullptr;
+	}
+	return *this;
+}
+
+//Vec<int> getVec(istream& is){}
+
 int main()
 {
-	Vec<int> v;
-	for (int i = 0; i < 1000; i++)
-		v.push_back(rand());
-	for (int i : v)
-		cout << i << endl;
+	Vec<int> v1,v2;
+	int i = 5;
+	v1.push_back(1);  //移动push_back
+	v1.push_back(i);  //拷贝push_back
+	//移动构造函数和拷贝构造函数通过函数匹配确定
+	v2 = v1;  //v1是左值，拷贝
+
+	//getVec返回右值，移动
+	//当没有移动构造函数时，也能调用拷贝
+	//v2 = getVec(cin);
+
+	cout<<"bye!"<<endl;
 	return 0;
 }
